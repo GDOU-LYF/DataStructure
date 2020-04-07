da6ffbfb166be126f97fae63f59d601c21cbbd24
LYF <liuyuanfeng1@stu.gdou.edu.cn>
2020-04-07 17:40:00 +0800

    1.2(2)斐波那契数列

---
+++ Fibonacci/Fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int Fibnoacci1(int n){ //时间复杂度 2N; 空间复杂度 N+1
+    //int F0=0,F1=1;
+    if(n<=1){
+        return 1;
+    }
+    return Fibnoacci1(n-1)+Fibnoacci1(n-2);
+}
+
+int Fibnoacci2(int n){//时间复杂度 N,空间复杂度 1
+    int f=1,g=0;
+    while(0<n--){
+        g+=f;
+        f=g-f;
+    }
+    return g;
+}
+int main(){
+    //Fibnoacci1(3);
+   //printf("%d",Fibnoacci1(10));
+    return 0;
+}
