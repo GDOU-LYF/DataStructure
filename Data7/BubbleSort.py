4f811e92bfee7c610a367dfa979a697aa771d44a
LYF <liuyuanfeng1@stu.gdou.edu.cn>
2020-05-23 17:18:38 +0800

    5-23

---
+++ Data7/BubbleSort.py
@@ -0,0 +1,19 @@
+def Bubble_Sort(our_list):
+    our_sort_list=our_list.copy()
+    for i in range(len(our_sort_list)):
+        for j in range(len(our_sort_list)-1):
+            if our_sort_list[j]>our_sort_list[i]:
+                our_sort_list[i],our_sort_list[j]=\
+                our_sort_list[j],our_sort_list[i]
+    return our_sort_list
+
+def main( ):
+    our=list("125438976")
+    out=list()
+    for i in map(eval,our):
+        out.append(i)
+    print(out)
+    print(Bubble_Sort(out))
+    print(out)
+    return 0
+main()
