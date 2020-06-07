82b617b8380cd5939718518751f5741a97dba7ea
LYF <liuyuanfeng1@stu.gdou.edu.cn>
2020-06-07 16:49:48 +0800

    归并排序

--- Data7/Merge.c
+++ Data7/Merge.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 void Merge(int a[],int tempA[],int L,int R,int RightEnd){
     int LeftEnd,Num,temp;
     int i;
@@ -13,16 +14,42 @@ void Merge(int a[],int tempA[],int L,int R,int RightEnd){
         }
     }
     while(L<=LeftEnd){
-        tempA[temp++]=a[L++];
+        tempA[temp++]=a[L++];//将剩下的内容复制到数组里
     }
     while(R<=RightEnd){
         tempA[temp++]=a[R++];
     }
-
-
+    for(i=0;i<Num;i++,RightEnd--){
+        a[RightEnd]=tempA[RightEnd];//复制回去
+    }
 }
 
+void MSort (int a[],int tempA[],int L,int RightEnd){
+    int Center;
+    if(L<RightEnd){
+        Center=(L+RightEnd)/2;
+        MSort(a,tempA,L,Center);
+        MSort(a,tempA,Center+1,RightEnd);
+        Merge(a,tempA,L,Center+1,RightEnd);
+    }
+}
+
+void MergeSort(int a[],int n){//归并排序
+    int *tempa;
+    tempa=(int *)malloc(n*sizeof(int));
+    if(tempa!=NULL){
+        MSort(a,tempa,0,n-1);
+        free(tempa);
+    }else{
+        printf("空间不足!");
+    }
+}
 
 int main(){
+    int a[]={44,12,59,36,62,43,94,7,35,52,85};
+    MergeSort(a,sizeof(a)/sizeof(int));//归并排序
+    for (int i=0;i<sizeof(a)/sizeof(int);i++){
+        printf("%d\t",a[i]);
+    }
     return 0;
 }
